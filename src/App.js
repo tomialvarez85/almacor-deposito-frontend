0936c989a848e32ae7da02c83fdd9781887ad89c
tomialvarez85 <tomialvarez78@gmail.com>
2023-01-16 16:37:26 -0300

    second commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
+import axios from 'axios';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+
+  const [depositos, setDepositos] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://127.0.0.1:8000/api/healthinsurances/')
+        .then(res => {
+            console.log(res.data)
+            setDepositos(res.data)
+        }).catch(err => {
+            console.log(err)
+        })
+  }, [])
+
+
+  const DoSomething = () => {
+    document.getElementById('sz').style.display = 'block';
+  }
+
+  const DoOtherThing = () => {
+    document.getElementById('next').style.display = 'inline-block';
+  }
+
   return (
     <div className="App">
       <h1>Seleccion de zona a trabajar</h1>
       <div>
         <h3>Seleccione Deposito</h3>
-        <select>
-          <option>Manzana</option>
-          <option>Pera</option>
+        <select name="healthinsurance" onBlur={DoSomething}>
+						<option selected>Obra social</option>
+            {
+              depositos.map(deposito => (
+                <option key={deposito.id} value={deposito.id}>{deposito.name}</option>
+              ))
+            }
         </select>
       </div>
       <div>
         <h3>Seleccione Zona</h3>
-        <select>
+        <select id="sz" className="select-zone" onBlur={DoOtherThing}>
           <option>Todas</option>
           <option>Carpa</option>
           <option>Monitor</option>
         </select>
       </div>
-      <a href="/selectoperation">Siguiente</a>
+      <a id="next" href="/selectoperation">Siguiente</a>
     </div>
   );
 }
